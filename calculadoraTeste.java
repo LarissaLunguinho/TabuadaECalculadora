3469377b11d31ef7897a1d3ad5bfd119e3151431
Larissa Lunguinho <84471388+LarissaLunguinho@users.noreply.github.com>
2021-06-26 20:32:35 -0300

    Update calculadoraTeste.java

--- calculadoraTeste.java
+++ calculadoraTeste.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 import com.sun.source.tree.YieldTree;
 
-// Crie um programa de uma calculadora utilizando uma instruÁ„o switch-case
+// Crie um programa de uma calculadora utilizando uma instru√ß√£o switch-case
 public class calculadoraTeste {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner (System.in);
 				
 	System.out.print ("1) Soma \n"
-				+ "2) SubstraÁ„o \n"
-				+ "3) Divis„o \n"
-				+ "4) MultiplicaÁ„o \n");
+				+ "2) Substra√ß√£o \n"
+				+ "3) Divis√£o \n"
+				+ "4) Multiplica√ß√£o \n");
 	
 					int escolha = s.nextInt();
 	
-	System.out.print ("Digite o n˙mero 1");
+	System.out.print ("Digite o n√∫mero 1");
 	int A = s.nextInt();
-	System.out.print("Digite o n˙mero 2");
+	System.out.print("Digite o n√∫mero 2");
 	int B = s.nextInt();
 	
 	switch (escolha) {
@@ -27,24 +27,15 @@ public class calculadoraTeste {
 		break;
 	case 2: 
 		int D = A-B;
-		System.out.print("SUBSTRA«√O= " +D);
+		System.out.print("SUBSTRA√á√ÉO= " +D);
 		break;
 	case 3:
 		int E = A/B;
-		System.out.print("DIVIS√O= "+E);
+		System.out.print("DIVIS√ÉO= "+E);
 		break;
 	case 4:
 		int F = A * B;
-		System.out.print("MultiplicaÁ„o= "+F);
+		System.out.print("Multiplica√ß√£o= "+F);
 		break;
 	
-	}
-	
-	
-	
-	
-	
-	
-	}}
-	
-	
+	}}}
